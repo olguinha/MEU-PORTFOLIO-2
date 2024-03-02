a3470d0d56e57d4f895246b53eb4fba8d2237c95
Olga Oliveira <olga93625@gmail.com>
2024-03-02 16:03:05 -0300

    mais uma parte

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
  import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormacaoComponent } from './formacao/formacao.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { HabilidadeComponent } from './habilidade/habilidade.component';
 
-const routes: Routes = [];
+
+
+export const routes: Routes = [
+  { path: '/',  component:  FormacaoComponent },
+  { path: '/*',  component:  SobreComponent },
+  { path: '//',  component:  HabilidadeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+  
+  
 })
 export class AppRoutingModule { 
   
